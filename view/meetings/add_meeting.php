b22208092bcf972c1bf5d021a11611671698c02d
Itsnope <noverjkd@gmail.com>
2025-05-18 00:47:16 +0700

    semantic class

--- view/meetings/add_meeting.php
+++ view/meetings/add_meeting.php
@@ -6,7 +6,7 @@
   <title>Add Meeting</title>
   <style>
 
-    .add-meet {
+    .meeting-editor {
       display: flex;
       width: 100%;
       padding: 30px 0;
@@ -16,30 +16,30 @@
       align-items: center;
     }
 
-    .form-page {
+    .meeting-form-container {
       display: flex;
       gap: 25px 0;
       flex-direction: column;
       width: 80%;
     }
 
-    .form-input {
+    .input-group {
       margin: 30px 0;
     }
 
-    .two-form {
+    .row-inputs {
       display: flex;
       gap: 0 30px;
     }
 
-    .form-user {
+    .input-field {
       display: flex;
       width: 100%;
       flex-direction: column;
       gap: 8px 0;
     }
 
-    .form-user input {
+    .input-field input {
       height: 48px;
       font-size: 1rem;
       padding: 10px;
@@ -47,22 +47,20 @@
       border-radius: 8px;
     }
 
-    .form-user textarea {
+    .input-field textarea {
       padding: 10px;
       border: 1px solid black;
       border-radius: 8px;
     }
 
-    .tombol {
+    .meeting-btn {
       display: flex;
       justify-content: flex-end;
       gap: 20px;
       margin: 50px 0;
     }
 
-
-
-    .tombol button {
+    .meeting-btn button {
       width: 150px;
       padding: 10px;
       font-size: 15px;
@@ -73,13 +71,13 @@
       opacity: 0.9;
     }
 
-    .tombol-main {
+    .meeting-btn-main {
       border: 1px solid #008080;
       background-color: #2a5d84;
       color: #fff;
     }
 
-    .tombol-dua {
+    .meeting-btn-dua {
       border: 1px solid gray;
       background-color: #fff;
       color: #000;
@@ -97,18 +95,18 @@
     }
 
     @media (max-width: 768px) {
-      .two-form {
+      .row-inputs {
         flex-direction: column;
         gap: 20px 0; /* gap vertikal 20px, horizontal 0 */
       }
-      .single-form {
+      .full-width-input {
         margin: 15px 0;
       }
-      .tombol {
+      .meeting-btn {
         flex-direction: column;
         gap: 20px 0;
       }
-      .tombol button {
+      .meeting-btn button {
         width: 100%;
       }
     }
@@ -117,60 +115,60 @@
 </head>
 <body>
 
-  <main class="add-meet">
-    <section class="title">
+  <main class="meeting-editor">
+    <section class="page-header">
       <h1>Add e-Meeting</h1>
     </section>
-    <section class="form-page">
+    <section class="meeting-form-container">
       <form action="app.php?page=process_meeting" method="POST">
-        <div class="form-input two-form">
-          <div class="form-user">
+        <div class="input-group row-inputs">
+          <div class="input-field">
             <label for="title">
               Judul Meeting
             </label>
             <input type="text" name="title" id="title" required>
           </div>
-          <div class="form-user">
+          <div class="input-field">
             <label for="location">
               Lokasi
             </label>
             <input type="text" name="location" id="location">
           </div>
         </div>
-        <div class="form-input single-form">
-          <div class="form-user">
+        <div class="input-group full-width-input">
+          <div class="input-field">
             <label for="description">
               Deskripsi
             </label>
             <textarea name="description" id="description" rows="8"></textarea>
           </div>
         </div>
-        <div class="form-input two-form">
-          <div class="form-user">
+        <div class="input-group row-inputs">
+          <div class="input-field">
             <label for="start_date">
               Awal Meeting
             </label>
             <input type="datetime-local" name="start_date" id="start_date" required>
           </div>
-          <div class="form-user">
+          <div class="input-field">
             <label for="end_date">
               Akhir Meeting
             </label>
             <input type="datetime-local" name="end_date" id="end_date" required>
           </div>
         </div>
-        <div class="form-input single-form">
-          <div class="form-user">
+        <div class="input-group full-width-input">
+          <div class="input-field">
             <label for="email">
               Guest E-mail
             </label>
-            <input type="email" name="guest" id="email" multiple>
+            <input type="email" name="guest" id="email" placeholder="Guest E-mail (pisahkan dengan koma jika lebih dari satu)" multiple>
           </div>
         </div>
 
-        <div class="tombol">
-          <button class="tombol-dua" onclick="window.location.href='app.php?page=list_meetings';return false;">Batal</button>
-          <button class="tombol-main" type="submit">Buat E-meeting</button>
+        <div class="meeting-btn">
+          <button class="meeting-btn-dua" onclick="window.location.href='app.php?page=list_meetings';return false;">Batal</button>
+          <button class="meeting-btn-main" type="submit">Buat E-meeting</button>
         </div>
       </form>
 
